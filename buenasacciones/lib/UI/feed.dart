2846d92fd4b1c8c4a720d5b2d0359d3acbd3b792
pablomdd <pablo.mdd.007@gmail.com>
2019-10-27 01:40:33 -0500

    agreado cards al feed, tipografias e imagenes al proyect

--- buenasacciones/lib/UI/feed.dart
+++ buenasacciones/lib/UI/feed.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'Widgets/bottomNavyBar.dart';
+import 'Widgets/trending_item.dart';
 
 class FeedScreen extends StatefulWidget {
   @override
@@ -9,17 +10,107 @@ class FeedScreen extends StatefulWidget {
 
 class _FeedScreenState extends State<FeedScreen> {
     var _selectedIndex = 0;
-  @override
+    final TextEditingController _searchControl = new TextEditingController();
+
+    @override
   Widget build(BuildContext context) {
 
     PageController _pageController = PageController();
 
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color.fromRGBO(77, 124, 174, 1),
         elevation: 1,
-        title: Text("Buenas Acciones"),
+//        title: Text("GoodDeeds",
+//                  style: TextStyle(
+//                    fontWeight: FontWeight.bold,
+//                    fontFamily: "Noto Sans",
+//                  ),
+//        ),
+          title: Image.asset("lib/assets/gooddeeds.jpeg"),
       ),
-      body: Text("Hola Mundo"),
+      body: Padding(
+          padding: EdgeInsets.fromLTRB(10.0,0,10.0,0),
+          child: ListView(
+            children: <Widget>[
+            Card(
+            elevation: 6.0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(
+                  Radius.circular(5.0),
+                ),
+              ),
+              child: TextField(
+                style: TextStyle(
+                  fontSize: 15.0,
+                  color: Colors.black,
+                ),
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.all(10.0),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(5.0),
+                    borderSide: BorderSide(color: Colors.white,),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.white,),
+                    borderRadius: BorderRadius.circular(5.0),
+                  ),
+                  hintText: "Buscar...",
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: Colors.black,
+                  ),
+                  suffixIcon: Icon(
+                    Icons.filter_list,
+                    color: Colors.black,
+                  ),
+                  hintStyle: TextStyle(
+                    fontSize: 15.0,
+                    color: Colors.black,
+                  ),
+                ),
+                maxLines: 1,
+                controller: _searchControl,
+              ),
+            ),
+          ),
+          TrendingItem(
+//            img: restaurant["img"],
+//            title: restaurant["title"],
+//            address: restaurant["address"],
+//            rating: restaurant["rating"],
+            img: "lib/assets/gooddeeds.jpeg",
+            title: "Reto Good Deeds Octubre",
+            address: "Bosque de Chapultepec",
+            rating: "5",
+          ),
+
+              TrendingItem(
+                img: "lib/assets/frapuccino.jpeg",
+                title: "Facebook Walk The City",
+                address: "Paseo de la Reforma",
+                rating: "4.5",
+              ),
+
+              TrendingItem(
+                img: null,
+                title: "Microsoft Hack Movility",
+                address: "Paseo de la Reforma",
+                rating: "4.7",
+              ),
+
+            ]),
+
+
+
+    ),
+
+
+
+
+
       bottomNavigationBar: BottomNavyBar(
         selectedIndex: _selectedIndex,
         showElevation: true, // use this to remove appBar's elevation
